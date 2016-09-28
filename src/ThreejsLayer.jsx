e8e786878a3532452fcf1ee339b9a80e2d30a308
Jordan Matelsky <j6k4m8@gmail.com>
2016-09-28 13:54:20 -0400

    remove jsx extension from imports (#17)

--- src/ThreejsLayer.jsx
+++ src/ThreejsLayer.jsx
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import Layer from './Layer.jsx';
+import Layer from './Layer';
 
 export default class ThreejsLayer extends Layer {
     /*
