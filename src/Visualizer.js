6727d06a3a65af80afd3bcfb8fb04feb0e5bb4ef
Joseph Downs <Joseph.Downs@jhuapl.edu>
2017-08-16 15:27:46 -0400

    fix: bind all visualizer function to the class

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -67,6 +67,14 @@ export default class Visualizer {
 
         this.onKeyDown = this.props.onKeyDown || (() => {});
         this.onClick = this.props.onClick || (() => {});
+        
+        // obligatory binding to class
+        this.animate = this.animate.bind(this);
+        this.getObjectsAtScreenCoordinate = this.getObjectsAtScreenCoordinate.bind(this);
+        this.init = this.init.bind(this);
+        this.requestUpdate = this.requestUpdate.bind(this);
+        this.setCameraLocRot = this.setCameraLocRot.bind(this);
+        this.triggerRender = this.triggerRender.bind(this);
     }
 
     requestUpdate() {
