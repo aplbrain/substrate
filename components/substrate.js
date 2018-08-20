5205376c65c9e4fc70c3a3046beeb15e3cf39705
Jordan Matelsky <j6k4m8@gmail.com>
2018-08-20 14:55:46 -0400

    Move TrackballControls into controls/ dir

--- components/substrate.js
+++ components/substrate.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _three = require('three');
+
+var THREE = _interopRequireWildcard(_three);
+
 var _Visualizer = require('./Visualizer');
 
 var _Visualizer2 = _interopRequireDefault(_Visualizer);
@@ -14,15 +18,14 @@ var _Layer2 = _interopRequireDefault(_Layer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 exports.default = {
     Visualizer: _Visualizer2.default,
     Layer: _Layer2.default
 };
 
 
-if (!window.THREE) {
-    throw "You must import THREE >= 0.";
-}
-
+window.THREE = THREE;
 window.Visualizer = _Visualizer2.default;
 window.Layer = _Layer2.default;
