eb178411f1f481296cd1bf850eaa7e25f2e5a233
Jordan Matelsky <j6k4m8@gmail.com>
2020-10-01 17:38:13 -0400

    Remove old example code

--- index.js
+++
@@ -1,8 +0,0 @@
-import Visualizer from './components/Visualizer';
-import Layer from './components/Visualizer';
-
-
-((scope) => {
-    window.Visualizer = Visualizer;
-    window.Layer = Layer;
-})(this);
